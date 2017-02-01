3d3bb5b43d9c1a26fec0014e2a5c10489299878b
Barkpuppy1 the overlord of masterfreedom <bbarked@gmail.com>
2017-02-01 16:44:29 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require_once("lib/Markdown_Parser.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>xBarkPuppy1 Website index</title>
+        <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+        <link href="markdown.css" media="all" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <article>
+	    <?php echo Markdown(file_get_contents("index.md")); ?>
+        </article>
+    </body>
+</html>
